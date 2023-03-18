9c617337e8e8de158737c8fa9df3ea643ff9cc76
Mateusz <zubermateusz@gmail.com>
2023-03-18 18:18:47 +0100

    Stage 2/4: Words and sentences

--- task/src/readability/Main.java
+++ task/src/readability/Main.java
@@ -5,11 +5,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        //. ! ?
+        // average > 10 HARD
+        // average <= 10 EASY
         Scanner scanner = new Scanner(System.in);
-        if(scanner.nextLine().length() > 100){
-            System.out.println("HARD");
-        } else {
-            System.out.println("EASY");
+
+        String[] sentences = scanner.nextLine().split("[.?!]"); // add every sentence to table
+
+        int[] wordCounter = new int[sentences.length];
+        for (int i = 0; i < sentences.length; i++) {
+            wordCounter[i] = sentences[i].split(" ").length;
         }
+
+        int sum = 0;
+        for (int j : wordCounter) {
+            sum += j;
+        }
+
+        String output = sum / wordCounter.length > 10 ? "HARD" : "EASY";
+        System.out.println(output);
     }
 }
