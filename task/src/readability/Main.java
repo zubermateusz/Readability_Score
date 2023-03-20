4be2c6b78486d932d3df489edcd5077aad7b9a53
Mateusz <zubermateusz@gmail.com>
2023-03-20 16:42:00 +0100

    add score and output text

--- task/src/readability/Main.java
+++ task/src/readability/Main.java
@@ -1,28 +1,54 @@
 package readability;
 
-import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
         //. ! ?
         // average > 10 HARD
         // average <= 10 EASY
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(new File(args[0]));
+        StringBuilder text = new StringBuilder();
 
-        String[] sentences = scanner.nextLine().split("[.?!]"); // add every sentence to table
+        while (scanner.hasNext()) {
+            text.append(scanner.nextLine());
+        }
+        scanner.close();
 
-        int[] wordCounter = new int[sentences.length];
-        for (int i = 0; i < sentences.length; i++) {
-            wordCounter[i] = sentences[i].split(" ").length;
+        System.out.println("The text is:");
+        System.out.println(text);
+
+        List<String> sentences = Arrays.asList(text.toString().split("[.?!]"));// add every sentence to table
+        List<String> words = Arrays.asList(text.toString().split(" ")); // add every character to table
+        int characters = text.toString().replaceAll(" ", "").length(); // add every character to table
+
+        System.out.println("Words: " + words.size());
+        System.out.println("Sentences: " + sentences.size());
+        System.out.println("Characters: " + characters);
+
+        double score = (4.71 * ((double)characters/ words.size()) + 0.5 * (double)words.size()/ sentences.size() - 21.43);
+        System.out.format("The score is: %.2f%n", score);
+
+        String outputYears = "";
+        int tempScore = (int) Math.ceil(score);
+        if (tempScore <= 13) {
+            outputYears = "" + tempScore + 4 + "-" + tempScore + 5;
+        } else if (tempScore == 14) {
+            outputYears = "" + tempScore + 4 + "-" + tempScore + 8;
         }
 
-        int sum = 0;
-        for (int j : wordCounter) {
-            sum += j;
-        }
 
-        String output = sum / wordCounter.length > 10 ? "HARD" : "EASY";
-        System.out.println(output);
+
+        System.out.println("This text should be understood by " + outputYears+ " year-olds.");
+        /*
+        //String output = sum / wordCounter.length > 10 ? "HARD" : "EASY";
+        //System.out.println(output);
+         */
+
+
+
     }
 }
