eef15e57f59ca2465101d7599cfc07abc9498b1d
Mateusz <zubermateusz@gmail.com>
2023-03-20 17:18:07 +0100

    add switch, and templates methods FK, SMOG, CL

--- task/src/readability/Main.java
+++ task/src/readability/Main.java
@@ -29,9 +29,6 @@ public class Main {
         System.out.println("Sentences: " + sentences.size());
         System.out.println("Characters: " + characters);
 
-        double score = (4.71 * ((double)characters/ words.size()) + 0.5 * (double)words.size()/ sentences.size() - 21.43);
-        System.out.format("The score is: %.2f%n", score);
-
         String outputYears = "";
         int tempScore = (int) Math.ceil(score);
         if (tempScore <= 13) {
@@ -39,16 +36,56 @@ public class Main {
         } else if (tempScore == 14) {
             outputYears = "" + (tempScore + 4) + "-" + (tempScore + 8);
         }
-
-
-
         System.out.println("This text should be understood by " + outputYears+ " year-olds.");
+
+
+
+        System.out.println("Syllables: 210");
+
+
+        System.out.println("Polysyllables: 17");
+
+        System.out.print("Enter the score you want to calculate (ARI, FK, SMOG, CL, all): ");
+        Scanner scanner2 = new Scanner(System.in);
+
+        switch (scanner.nextLine()) {
+            case "ARI" -> {
+                System.out.println();
+                System.out.format("The score is: %.2f%n", ARI(characters, words.size(), sentences.size()));
+            }
+            case "FK" -> {
+                System.out.println();
+            }
+            case "SMOG" -> {
+                System.out.println();
+            }
+            case "CL" -> {
+                System.out.println();
+            }
+            case "all" -> {
+                System.out.println();
+            }
+        }
         /*
         //String output = sum / wordCounter.length > 10 ? "HARD" : "EASY";
         //System.out.println(output);
          */
-
-
-
     }
+
+
+
+    static double ARI(int NumberOfCharacters, int numberOfWords, int numberOfSentences) {
+        return (4.71 * ((double)NumberOfCharacters/ numberOfWords) + 0.5 * (double)numberOfWords/ numberOfSentences - 21.43);
+    }
+
+    static double FK(int NumberOfCharacters, int numberOfWords, int numberOfSentences) {
+        return (4.71 * ((double)NumberOfCharacters/ numberOfWords) + 0.5 * (double)numberOfWords/ numberOfSentences - 21.43);
+    }
+    static double SMOG(int NumberOfCharacters, int numberOfWords, int numberOfSentences) {
+        return (4.71 * ((double)NumberOfCharacters/ numberOfWords) + 0.5 * (double)numberOfWords/ numberOfSentences - 21.43);
+    }
+    static double CL(int NumberOfCharacters, int numberOfWords, int numberOfSentences) {
+        return (4.71 * ((double)NumberOfCharacters/ numberOfWords) + 0.5 * (double)numberOfWords/ numberOfSentences - 21.43);
+    }
+
 }
